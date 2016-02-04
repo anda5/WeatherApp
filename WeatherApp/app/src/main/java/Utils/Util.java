0dff9c374b923526ce99fedac015df2f12f87bd8
Anda Lixandroiu <lix.anda@yahoo.com>
2016-02-04 04:31:50 +0200

    Add new method getJSONObject

--- WeatherApp/app/src/main/java/Utils/Util.java
+++ WeatherApp/app/src/main/java/Utils/Util.java
@@ -6,18 +6,23 @@ import android.util.Log;
 
 import com.anda.weatherapp.R;
 
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Created by anda on 2/4/2016.
  */
 public class Util {
 
-    private static final String URL = "api.openweathermap.org/data/2.5/weather?q=";
-    private static final String HEADER = "x-api-key";
-    private static final String ICON = "api.openweathermap.org/img/w/";
+    private static final String _URL = "api.openweathermap.org/data/2.5/weather?q=";
+    private static final String _HEADER = "x-api-key";
+    private static final String _ICON = "api.openweathermap.org/img/w/";
 
     private static String getApiKey(Context context){
         String result = null;
@@ -31,5 +36,34 @@ public class Util {
             Log.v("Error","Can't open the file");
         }
       return result;
+    }
+
+    public static String getJSONObject(Context context, String location){
+        HttpURLConnection connection = null;
+        InputStream inputStream = null;
+
+        try {
+            connection = (HttpURLConnection)(new URL(_URL+location)).openConnection();
+            connection.setRequestMethod("GET");
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            connection.connect();
+
+            StringBuffer stringBuffer = new StringBuffer();
+            inputStream = connection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = null;
+            while((line=bufferedReader.readLine())!=null){
+             stringBuffer.append(line+"\r\n");
+            }
+            inputStream.close();
+            connection.disconnect();
+
+            return stringBuffer.toString();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+       return  null;
     }
 }
